9549c38d7adf9b4346eee526ec6cdbcb8d354982
Alberto Espinosa <110586852+albertoysaac@users.noreply.github.com>
2024-03-05 03:20:40 +0000

    generador de dominios completado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domains = domainGenerator(pronoun, adj, noun);
+  allDom(domains);
+  rDom(domains);
 };
+
+function domainGenerator(pronoun, adj, noun) {
+  let domain = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        domain.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+  return domain;
+}
+function random() {
+  let domains = domainGenerator(pronoun, adj, noun);
+  rDom(domains);
+}
+
+function allDom(domains) {
+  let ul = document.querySelector(".domains ul");
+  for (let i = 0; i < domains.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = domains[i];
+    ul.appendChild(li);
+  }
+}
+function rDom(domains) {
+  let rDom = document.querySelector(".input");
+  let randomDomain = domains[Math.floor(Math.random() * domains.length)];
+  rDom.innerHTML = `${randomDomain}`;
+}
+window.random = random;
